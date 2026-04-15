0c863f9a38d551ba900a3ba91bba7c7f2d7f564a
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-04-15 13:15:47 +0100

    Rendering data into the UI

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 //You can edit ALL of the code here
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+
+  const rootElem = document.getElementById("root");
+
+  const template = rootElem.querySelector("#card");
+
+  const episodes = getAllEpisodes();
+
+  const createEpisodeCard = (episode) => {
+    const card = template.content.cloneNode(true);
+
+    const link = card.querySelector(".episode-link");
+    link.href = episode.url;
+
+    const s = String(episode.season).padStart(2, "0");
+    const e = String(episode.number).padStart(2, "0");
+
+    card.querySelector(".name").textContent = `${episode.name} - S${s}E${e}`;
+    card.querySelector("img").src = episode.image.medium;
+    card.querySelector(".summary").innerHTML = episode.summary;
+
+    return card;
+  }
+
+  const episodeCards = episodes.map(createEpisodeCard);
+
+  rootElem.append(...episodeCards);
+
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
 window.onload = setup;
