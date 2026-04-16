fa0c170413500c3862e86c961037f11147fa8237
ofonimeedak <ofonimeedak@gmail.com>
2026-04-16 19:55:45 +0100

    give more descriptive variable name

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //You can edit ALL of the code here
 function setup() {
-
   const rootElem = document.getElementById("root");
 
   const template = rootElem.querySelector("#card");
@@ -13,21 +12,20 @@ function setup() {
     const link = card.querySelector(".episode-link");
     link.href = episode.url;
 
-    const s = String(episode.season).padStart(2, "0");
-    const e = String(episode.number).padStart(2, "0");
+    const episodeSeason = String(episode.season).padStart(2, "0");
+    const episodeNo = String(episode.number).padStart(2, "0");
 
-    card.querySelector(".name").textContent = `${episode.name} - S${s}E${e}`;
+    card.querySelector(".name").textContent =
+      `${episode.name} - S${episodeSeason}E${episodeNo}`;
     card.querySelector("img").src = episode.image.medium;
     card.querySelector(".summary").innerHTML = episode.summary;
 
     return card;
-  }
+  };
 
   const episodeCards = episodes.map(createEpisodeCard);
 
   rootElem.append(...episodeCards);
-
 }
-
 
 window.onload = setup;
